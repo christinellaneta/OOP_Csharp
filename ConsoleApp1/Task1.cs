1904da343cece2b5fe89ef632686eeb4fff58eec
christinellaneta <llaneta.christinemae@gmail.com>
2025-12-04 16:19:31 +0800

    modified:   ConsoleApp1/ConsoleApp1.csproj 	modified:   ConsoleApp1/Task1.cs

--- ConsoleApp1/Task1.cs
+++ ConsoleApp1/Task1.cs
@@ -2,15 +2,12 @@
 
 namespace ConsoleApp1
 {
-    // Task 1: This class contains the single application entry point (Main).
     public class Task1
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Kamusta, Mundo!");
             
-            // -----------------------------------------------------------------
-            // Next Task Execution: Dispatching to the next class
             Console.WriteLine("\n--- Preparing to execute Task 2 logic... ---");
             Task2.RunTask(); 
             
